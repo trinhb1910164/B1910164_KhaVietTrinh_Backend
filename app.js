0ce31da8196f8327e4962e961c7cbf2a2bfe136a
khaviettrinh <trinhb1910164@student.ctu.edu.vn>
2023-02-23 16:36:12 +0700

    .

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const contactsRouter = require("./app/routes/contact.route");
-const cors = require("cors");
 const ApiError = require("./app/api-error");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use("/api/contacts", contactsRouter);
-// app.use(express.json());
 
 // handle 404 response
 app.use((req, res, next) => {
@@ -15,13 +15,14 @@ app.use((req, res, next) => {
     // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
     return next(new ApiError(404, "Resource not found"));
 });
-// define error-handling middleware last, after other app.use() and routes calls
+
+    // define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
     // Middleware xử lý lỗi tập trung.
     // Trong các đoạn code xử lý ở các route, gọi next(error)
     // sẽ chuyển về middleware xử lý lỗi này
     return res.status(err.statusCode || 500).json({
-        message: err.message || "Internal Server Error",
+    message: err.message || "Internal Server Error",
     });
 });
 
